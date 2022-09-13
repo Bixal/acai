37f4226305ea07d269d2baa417f0efb23645a480
John Franklin <john.franklin@bixal.com>
2022-09-13 16:01:46 -0400

    Add copyright and GPLv3 license information.

--- acai/acai.py
+++ acai/acai.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 
+# Copyright 2022 John Franklin <john.franklin@bixal.com>
+# Copyright 2022 Bixal Solutions, inc.
+#
+# Licensed under the GPL, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.gnu.org/licenses/gpl-3.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """ACAI - Acquia Cert Automatic Installer
 
 This program installs LetsEncrpt certs into Acquia an environment defined in
